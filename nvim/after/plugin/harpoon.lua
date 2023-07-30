1f59bc937cdc7623a68cae3ad258e07efb949bcf
Trong Nhan Mai <trongnhan061199@gmail.com>
2023-07-30 15:44:35 +1000

    change keymap of harpoon because of conflict with vim-tmux-navigator

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -5,7 +5,8 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+# Note that Ctrl+h is already used by vim-tmux-navigator
+vim.keymap.set("n", "<C-i>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
