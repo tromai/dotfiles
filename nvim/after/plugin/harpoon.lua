75b70617cfb746097a3a1674f47286a24e72499e
Trong Nhan Mai <trongnhan061199@gmail.com>
2023-07-30 15:47:30 +1000

    change comment symbol in harpoon.lua

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -5,7 +5,7 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-# Note that Ctrl+h is already used by vim-tmux-navigator
+-- Note that Ctrl+h is already used by vim-tmux-navigator
 vim.keymap.set("n", "<C-i>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
