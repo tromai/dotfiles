547234959aa32ba03c38f34b1ce38f2b0678344f
Trong Nhan Mai <33509400+tromai@users.noreply.github.com>
2026-04-25 11:36:01 +1000

    new minimal nvim setup

--- nvim/lua/tromai/core/init.lua
+++
@@ -1,4 +0,0 @@
-require 'tromai.core.loadfirst'
-require 'tromai.core.editor'
-require 'tromai.core.remap'
-require 'tromai.core.autocommands'
--- nvim/lua/tromai/init.lua
+++
@@ -1,2 +0,0 @@
-require 'tromai.core'
-require 'tromai.plugins'
